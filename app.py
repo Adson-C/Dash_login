ff7b8da3c7e2cb3530ef82aa38ae1661a9a51714
Adson Conceição <adsonconcecao@yahoo.com.br>
2024-09-15 18:50:24 -0300

    Iniciando Projeto tela Login

---
+++ app.py
@@ -0,0 +1,8 @@
+from sqlalchemy.sql import select
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin
+import configparser
+import os
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.QUARTZ])
+server = app.server
